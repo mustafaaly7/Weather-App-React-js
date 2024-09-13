8d6d3e20e59e463c6fe13753a34de21b7a75c569
Mustafa Ali <mustafaaaly27@gmail.com>
2024-09-14 00:27:52 +0500

    mobile responsivness

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -15,25 +15,25 @@ return(
     <>
    
   
-    <div className=" bg-opacity-80 p-10 rounded-lg shadow-lg text-center">
+    <div className="w-full bg-opacity-80 p-10 rounded-lg shadow-lg text-center">
       
       <h1 className="text-3xl font-bold underline my-4">{name}</h1>
       <h2 className="text-2xl mb-4">
         {temp}°C (Feels like: {feels_like}°C)
       </h2>
-      <div className="text-xl my-2 flex justify-between">
+      <div className="text-xl my-2 flex justify-around">
         <span className="font-semibold">Humidity:</span>
         <span>{main?.humidity}%</span>
       </div>
-      <div className="text-xl my-2 flex justify-between">
+      <div className="text-xl my-2 flex justify-around">
         <span className="font-semibold">Pressure:</span>
         <span>{main?.pressure} hPa</span>
       </div>
-      <div className="text-xl my-2 flex justify-between">
+      <div className="text-xl my-2 flex justify-around">
         <span className="font-semibold">Sunrise:</span>
         <span>{new Date(sys?.sunrise * 1000).toLocaleTimeString()}</span>
       </div>
-      <div className="text-xl my-2 flex justify-between">
+      <div className="text-xl my-2 flex justify-around">
         <span className="font-semibold">Sunset:</span>
         <span>{new Date(sys?.sunset * 1000).toLocaleTimeString()}</span>
       </div>
