1d367beeaf12e0f9571981a9365e829052ecc465
Mustafa Ali <mustafaaaly27@gmail.com>
2024-09-13 23:32:04 +0500

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Weather } from './components/weather';
 import './App.css'
 
 function App() {
@@ -31,16 +32,30 @@ function App() {
     "Chittagong",    // Bangladesh
     "Sylhet"         // Bangladesh
   ];
-  const [loader, setLoader] = useState(true)
+  const [weatherData,setWeatherData] = useState([])
+  const [loader, setLoader] = useState(false)
   const [selectedCity, setSelectedCity] = useState(popularCities[0])
-  console.log(selectedCity);
+  // console.log(selectedCity);
 
 
   useEffect(() => {
+    setLoader(true)
+    console.log(loader);
+    
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=1f136667cfcdb418bf8b7a4c5a542f00`)
       .then((res) => res.json())
-.then((data)=>console.log(data)
-)
+      .then((data) => {
+        setWeatherData(data)
+        // console.log("weahter data",weatherData);
+        setLoader(false)
+        // console.log(selectedCity);
+        
+        
+
+        
+      }
+      ).catch((err)=>console.log(err)
+    )
   }, [selectedCity])
 
 
@@ -52,20 +67,35 @@ function App() {
 
 
       <h1 className="m-5 mx-auto text-6xl font-mono ">Weather App</h1>
-      <div className=" font-mono card max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className=" text-white font-mono card max-w-lg max-h-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
         <div className="p-5">
-
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+          <h5 className="mb-2 text-4xl font-mono   ">
+            Search Your City
           </h5>
 
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so far, in
-            reverse chronological order.
-          </p>
+          <select name="cities" className='text-black' onChange={(e) => setSelectedCity(e.target.value)} >
+            {popularCities.map((name, index) => (
+              // <option  />
+              <option key={index} value={name} className='text-black' >{name}</option>
+            ))}
+
+          </select>
+
+
 
         </div>
+{loader?(
+  
+  <h1 className='text-white text-3xl my-6'>LOADING......</h1>
+):(
+
+<Weather weatherData={weatherData}/>
+  
+  
+  
+)}
+
       </div>
 
 
